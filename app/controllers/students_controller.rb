8c9548190ffa4dcc977b1f58b11a65eeef9833c7
jason <jasonedlewis@gmail.com>
2019-08-08 22:29:36 -0400

    student/new form built out and all paths and links working

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,29 @@
 class StudentsController < ApplicationController
 
     def index 
-
+        @students = Student.all  
     end
 
+   #show
+    def show
+        @student = Student.find(params[:id])
+        render :show
+    end
+   
+    #new 
     def new
-    
+        render :new
     end
+
+
+    
+    
+    #find student
+    def find_student
+        Student.find(params[:id])
+    end
+    
+
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+jason = Student.create(name: "jason", age: 12, grade: 7, location: "White Plains")
+Pete = Student.create(name: "pete", age: 14, grade: 9, location: "Bronx")
+Xavier = Student.create(name: "xavier", age: 15, grade: 10, location: "Brooklyn")
