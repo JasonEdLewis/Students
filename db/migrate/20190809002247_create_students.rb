95fb64a3a4b6b93e6e340f14ad14aac2b31e24c8
jason <jasonedlewis@gmail.com>
2019-08-08 20:57:25 -0400

    rails students app inception

---
+++ db/migrate/20190809002247_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.integer :age
+      t.integer :grade
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
