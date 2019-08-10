5e541b53e146ee4d5c2ca5279c8cbdd118a7a750
jason <jasonedlewis@gmail.com>
2019-08-09 20:29:45 -0400

    Finished Full CRUD on Students App

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,49 +2,54 @@ class StudentsController < ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :find_student, only: [:show, :edit, :update, :destroy]
     
-    #find student
-    def find_student
-        @student = Student.find(params[:id])
-    end
-
+    
     def index #NEEDS ALL STUDENTS
         @students = Student.all  
         
     end
-
+    
     #SHOW needs INSTANCE of logged STUDENT to display
     def show 
         # @student = Student.find(params[:id])
     end
-   
+    
     #NEW - FORM needs INSTANCE of Student.NEW , for UPDATE ACTION to instanciate a new student
     def new 
         @student = Student.new
-        render :new
     end
     
+    #create
+    def create
+         @student = Student.create(student_params)
+        redirect_to @student
+    end
+
+
+
     #EDIT - FORM to take updated information for STUDENT:id pass it into UPDATE
     def edit 
         # @student  = Student.new
     end
-
+    
     #UPDATE takes PARMAMS from EDIT FORM and updates Student:id info
     def update 
         # @student = Student.find(params[:id])
         @student.update(student_params)
         redirect_to @student
-
-    end
-    #create
-    def create
         
-        @student = Student.new(student_params)
-        @student.save
-        redirect_to @student
     end
 
-
-
+    def destroy
+        @student.destroy
+        redirect_to students_path
+    end
+    
+    
+    
+    #find student
+    def find_student
+        @student = Student.find(params[:id])
+    end
 
 
 
